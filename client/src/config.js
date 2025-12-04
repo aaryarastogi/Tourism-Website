31ead24c3c87815c09982b182d488c4b2e86b0a3
Aarya Rastogi <97822457+aaryarastogi@users.noreply.github.com>
2025-12-05 01:19:19 +0530

    backend hosted and route added in frontend

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-const backend_url = "http://localhost:8000";
+const backend_url = "https://tourism-website-backend-p8h2.onrender.com";
 export default backend_url;
 
 export const razorkey_id = "rzp_test_RfA1p0DJC2RGk4";
